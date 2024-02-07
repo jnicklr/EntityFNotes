195cdd87cd45479c7ce5cc8f3c2f9f4011bc3771
jnicklr <jnichollaslr@gmail.com>
2024-02-06 21:26:10 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EntityFNotes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
