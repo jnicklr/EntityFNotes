794189b7e920f1f6afeddb3bbaef50d63d17f005
jnicklr <jnichollaslr@gmail.com>
2024-02-07 17:54:07 -0300

    build: Definindo as Configurações da Aplicação e Criando o DataContext.

---
+++ Data/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace EntityFNotes.Data
+{
+    public class DataContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public DataContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            Console.WriteLine(_configuration.GetConnectionString("DB"));
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DB"));
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,30 @@
-﻿namespace EntityFNotes
+﻿using EntityFNotes.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Data;
+
+namespace EntityFNotes
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Cria uma coleção de serviços.
+            ServiceCollection serviceCollection = new ServiceCollection();
+
+            // ConfigurationBuilder serve para criar configurações para o aplicativo.
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName) // Define o diretório base para a busca de arquivos da configuração.
+                .AddJsonFile("appsettings.json") // Adiciona o .json como fonte de configuração.
+                .Build(); // Constrói a instância de configuração final.
+
+            // Registrando as classes na coleção de serviços.
+            serviceCollection.AddSingleton<IConfiguration>(configuration);
+            serviceCollection.AddSingleton<DataContext>();
+
+            var serviceProvider = serviceCollection.BuildServiceProvider(); // O provedor de serviços é usado para obter as instâncias de serviços registrados.
+            var context = serviceProvider.GetService<DataContext>(); // Obtendo uma instância de DataContext.
+            
         }
     }
 }
